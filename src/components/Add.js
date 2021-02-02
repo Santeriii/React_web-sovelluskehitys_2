1f5c6b130ab2ae5d6ce0832017efef351328293d
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2021-02-02 15:00:48 +0200

    8. tehtävän sovellus valmis

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 
 const Add = () => {
+    const [newNote, setNewNote] = useState('')
+
+    const addNote = event => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: false,
+        }
+
+        axios
+            .post('http://localhost:3001/notes', noteObject)
+            .then(response => {
+                setNewNote('')
+            })
+    }
+
+    const changeNewNote = event => {
+        setNewNote(event.target.value)
+    }
 
     return(
         <div className="container">
            <div>
-               <p>Valinta 1.</p>
+               <TextField onChange={changeNewNote}/>
+               <Button onClick={addNote}>Send</Button>
            </div>
         </div>
     )
